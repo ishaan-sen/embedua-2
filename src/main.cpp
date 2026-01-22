28c2ddb1f58618b5d854bacf32892f52b47a44e3
Ishaan Sen <ishaan.sen100@gmail.com>
2026-01-22 14:13:01 -0500

    C++ drive code for new chassis

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,42 @@
-// #include <Arduino.h>
-// #include "uiua_servo.hpp"
-// #include "uiua.h"
+/*
 
-// void setup()
-// {
-//     Serial.begin(115200);
-//     Serial1.begin(115200);
-// }
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
 
-// void loop()
-// {
-//     if (Serial1.available())
-//     {
-//         Serial.write(Serial1.read());
-//     }
-// }
+Adafruit_MotorShield *get_motor_driver(uint8_t address) // 0x60 default
+{
+    Adafruit_MotorShield *driver = new Adafruit_MotorShield(address);
+    driver->begin();
+    return driver;
+}
+
+Adafruit_DCMotor *get_dc_motor(Adafruit_MotorShield *driver, uint8_t port)
+{
+    return driver->getMotor(port);
+}
+
+void set_dc_speed(Adafruit_DCMotor *motor, uint8_t speed)
+{
+    motor->setSpeed(speed);
+}
+
+void run_dc_motor(Adafruit_DCMotor *motor, uint8_t command)
+{
+    motor->run(command);
+}
+
+
+void setup(void) {
+    Adafruit_MotorShield *driver = get_motor_driver(0x60);
+    Adafruit_DCMotor *motors[4];
+    uint8_t dirs[] = {1, 2, 2, 2};
+    for (int i = 0; i < 4; i++){
+        motors[i] = get_dc_motor(driver, i+1);
+        set_dc_speed(motors[i], 128);
+        run_dc_motor(motors[i], dirs[i]);
+    }
+}
+
+void loop(){}
+
+*/
